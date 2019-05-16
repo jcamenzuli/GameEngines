fa2405dfd9a4e964f59401d58bf3ccbaed364cc0
fabianattard <43746478+fabianattard@users.noreply.github.com>
2019-05-16 20:52:48 +0200

    Fixed Bug

--- Ninja/Assets/Scripts/PlayerControls.cs
+++ Ninja/Assets/Scripts/PlayerControls.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {   
@@ -97,8 +98,14 @@ public class PlayerControls : MonoBehaviour
             if (other.gameObject.tag == "Switch")
             {
                 Switch switchScript = other.GetComponent<Switch>();
-                if (switchScript != switchInRange) switchInRange = null;
+                if (switchScript == switchInRange) switchInRange = null;
             }
         }
+    }
+
+
+    private void OnBecameInvisible()
+    {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
---
+++ Ninja/Assets/Scripts/SceneExample.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class SceneExample : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(SceneManager.GetActiveScene().name);
+    }
+}
